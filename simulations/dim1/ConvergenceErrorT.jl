26a72dbf0d87eeab966ccb8b6add3cbf91cdc800
theo-belin <theo.belin@proton.me>
2026-08-31 10:24:49 +0200

    convergence diagram with time-dependent exact solution

--- simulations/dim1/ConvergenceErrorT.jl
+++ simulations/dim1/ConvergenceErrorT.jl
@@ -15,58 +15,43 @@ mutable struct ProblemData
 	source::Any
 end
 
-# v, lambda and f for smooth steady state
-function source_smooth(node)
+# time-dependent coefficient
+function A(t)
+	m = 0.2 #should be chosen small enough to ensure time independence of u
+	m*t^2
+end
+
+# time-dependent source term f
+function source(node)
 	x = node[1]
-	pi^2*cos(pi*x)
+	t = node.time
+	A(t)*pi^2*cos(pi*x)
 end
 
-function v0_smooth(x)
-	cos(pi*x)
+# exact v solution
+function v_exact(t,x)
+	A(t)*cos(pi*x)
 end
 
-
-function lambda0_smooth(x)
+#exact u solutions
+function u_exact(kappa,x)
+	kappa0(v) = kappa(v,0)
+	kappa1(v) = kappa(v,1)
 	if x > -1/2 && x < 1/2
-		1.0
+		(kappa0(cos(pi*x)) + kappa1(0))/2
 	else
-		0.0
+		(kappa0(0) + kappa1(cos(pi*x)))/2
 	end
 end
 
-# v, lambda and f for the rough steady state
-function source_rough(node)
-	x = node[1]
-	if x > -1/2 && x < 1/2
-		2*pi^2*cos(2*pi*x)
-	else
-		0.0
-	end
+# exact lambda solution
+function lambda_exact(kappa,t,x)
+	kappa0(v) = kappa(v,0)
+	kappa1(v) = kappa(v,1)
+	(u_exact(kappa, x) - kappa0(v_exact(t,x)))/(kappa1(v_exact(t,x)) - kappa0(v_exact(t,x)))
 end
 
-function v0_rough(x)
-	if x > -1/2 && x < 1/2
-		1/2*cos(2*pi*x)
-	else
-		-1/2
-	end
-end
-
-function lambda0_rough(x)
-	if x > -1/4 && x < 1/4
-		1.0
-	else
-		0.0
-	end
-end
-
-function main(;type = "Smooth", visualize = true, test = false, p = 2)
-
-	if type == "Smooth"
-		source = source_smooth
-	else
-		source = source_rough
-	end
+function main(; visualize = true, test = false, p = 2)
 
 	problem_data_PWL = ProblemData(
 		ForwardBackwardHeat.PWLinear.phi, 
@@ -75,7 +60,7 @@ function main(;type = "Smooth", visualize = true, test = false, p = 2)
 		ForwardBackwardHeat.PWLinear.phi_0, 
 		source
 	)
-	K, lp_error_PWL, l2h1_error_PWL = convergence_error(problem_data_PWL; type)
+	K, lp_error_v_PWL, lp_error_u_PWL, l2h1_error_PWL = convergence_error(problem_data_PWL)
 
 	problem_data_Par = ProblemData(
 		ForwardBackwardHeat.Parallel.phi, 
@@ -84,7 +69,7 @@ function main(;type = "Smooth", visualize = true, test = false, p = 2)
 		ForwardBackwardHeat.Parallel.phi_0, 
 		source
 	)
-	_, lp_error_Par, l2h1_error_Par = convergence_error(problem_data_Par; type)
+	_, lp_error_v_Par, lp_error_u_Par, l2h1_error_Par = convergence_error(problem_data_Par)
 
 	problem_data_Cub = ProblemData(
 		ForwardBackwardHeat.Cubic.phi, 
@@ -93,24 +78,35 @@ function main(;type = "Smooth", visualize = true, test = false, p = 2)
 		ForwardBackwardHeat.Cubic.phi_0, 
 		source
 	)
-	_, lp_error_Cub, l2h1_error_Cub = convergence_error(problem_data_Cub; type)
+	_, lp_error_v_Cub, lp_error_u_Cub, l2h1_error_Cub = convergence_error(problem_data_Cub)
 
 	if visualize
-		p = GridVisualizer(; Plotter = Plots, layout = (1, 2), legend = :rt, fast = true, size = (1000, 600))
+		p = GridVisualizer(; Plotter = Plots, layout = (1, 3), legend = :rt, fast = true, size = (1000, 600))
+
 		scalarplot!(
-			p[1, 1], log10.(K.^(-1)), log10.(lp_error_PWL.^(-1)); label = "PWL", color = :blue,  
-			title = "Convergence error in L^2_{t,x}", xlabel = "-log(dt))", ylabel = "-log-Error",
+			p[1, 1], log10.(K.^(-1)), log10.(lp_error_v_PWL.^(-1)); label = "PWL", color = :blue, 
+			title = "Convergence error of v in L^2_{t,x}", xlabel = "-log(dt)", ylabel = "-log-Error",
 			markershape = :circle, markevery = 1
 		)
-		scalarplot!(p[1, 1], log10.(K.^(-1)), log10.(lp_error_Par.^(-1)); label = "Par", color = :green,clear = false)
-		scalarplot!(p[1, 1], log10.(K.^(-1)), log10.(lp_error_Cub.^(-1)); label = "Cub", color = :red, clear = false)
+		scalarplot!(p[1, 1], log10.(K.^(-1)), log10.(lp_error_v_Par.^(-1)); label = "Par", color = :green,clear = false)
+		scalarplot!(p[1, 1], log10.(K.^(-1)), log10.(lp_error_v_Cub.^(-1)); label = "Cub", color = :red, clear = false)
+
 		scalarplot!(
-			p[1, 2], log10.(K.^(-1)), log10.(l2h1_error_PWL.^(-1)); label = "PWL", color = :blue,
-			title = "Convergence error in L^2_tH^1_x", xlabel = "-log(dt)", ylabel = "-log-Error", 
+			p[1, 2], log10.(K.^(-1)), log10.(lp_error_u_PWL.^(-1)); label = "PWL", color = :blue, 
+			title = "Convergence error of u in L^2_{t,x}", xlabel = "-log(dt)", ylabel = "-log-Error",
 			markershape = :circle, markevery = 1
 		)
-		scalarplot!(p[1, 2], log10.(K.^(-1)), log10.(l2h1_error_Par.^(-1)); label = "Par", color = :green, clear = false)
-		scalarplot!(p[1, 2], log10.(K.^(-1)), log10.(l2h1_error_Cub.^(-1)); label = "Cub", color = :red, clear = false)
+		scalarplot!(p[1, 2], log10.(K.^(-1)), log10.(lp_error_u_Par.^(-1)); label = "Par", color = :green,clear = false)
+		scalarplot!(p[1, 2], log10.(K.^(-1)), log10.(lp_error_u_Cub.^(-1)); label = "Cub", color = :red, clear = false)
+
+		scalarplot!(
+			p[1, 3], log10.(K.^(-1)), log10.(l2h1_error_PWL.^(-1)); label = "PWL", color = :blue,
+			title = "Convergence error of v in L^2_tH^1_x", xlabel = "-log(dt)", ylabel = "-log-Error", 
+			markershape = :circle, markevery = 1
+		)
+		scalarplot!(p[1, 3], log10.(K.^(-1)), log10.(l2h1_error_Par.^(-1)); label = "Par", color = :green, clear = false)
+		scalarplot!(p[1, 3], log10.(K.^(-1)), log10.(l2h1_error_Cub.^(-1)); label = "Cub", color = :red, clear = false)
+
 		reveal(p)
 	end
 	
@@ -122,15 +118,7 @@ function main(;type = "Smooth", visualize = true, test = false, p = 2)
 	
 end
 
-function convergence_error(pb_data; type = "Smooth", p = 2)
-
-	if type == "Smooth"
-		v0_ini = v0_smooth
-		lambda0_ini = lambda0_smooth
-	else
-		v0_ini = v0_rough
-		lambda0_ini = lambda0_rough
-	end
+function convergence_error(pb_data; p = 2)
 
 	# Unpacking problem data
 	gamma_0, kappa = pb_data.gamma_eps, pb_data.kappa
@@ -143,21 +131,21 @@ function convergence_error(pb_data; type = "Smooth", p = 2)
 
 	T_max = 1
 
-	u0, v0, lambda0 = prepare_IC(
-		gamma_0, kappa, 
-		v0_ini, lambda0_ini
-	)
+	v0(x) = v_exact(0,x)
+	u0(x) = u_exact(kappa,x)
+	lambda0(x) = lambda_exact(kappa,0,x)
 	
 	# Sequence of time steps
 	K =[0.5, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005]
-	lp_error = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+	lp_error_v = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+	lp_error_u = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 	l2h1_error = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 	for i = 1:length(K)
 		k = K[i]
 
 		# Space discretisation
-		X = X_min:0.005:X_max
+		X = X_min:0.002:X_max
 		sgrid = simplexgrid(X)
 
 		# Time discretisation
@@ -168,26 +156,40 @@ function convergence_error(pb_data; type = "Smooth", p = 2)
 			sgrid, T, pb_data
 		)
 
-		lp_er = 0
+		lp_er_v = 0
+		lp_er_u = 0
 		l2h1_er = 0
 
-		for j in 1:(length(tu.t)-1)
+		tu_ex = unknowns(sys)
+		tv_ex = unknowns(sys)
+		tlambda_ex = unknowns(sys)
+		tu_ex[1,:] = map(u0, sgrid)
+
+		for j = 1:(length(tu.t)-1)
 			tj = tu.t[j]
 			tjp = tu.t[j + 1]
 			dt = tjp - tj
-			lp_er = lp_er + dt*(
-				VoronoiFVM.lpnorm(sys, tv(tj) - tv(k), p)^p
-				+ VoronoiFVM.lpnorm(sys, tu(tj) - tu(k), p)^p
-				+ VoronoiFVM.lpnorm(sys, tlambda(tj) - tlambda(k), p)^p
+
+			tv_ex[1,:] = map(x -> v_exact(tj, x), sgrid)
+			tlambda_ex[1,:] = map(x -> lambda_exact(kappa, tj, x), sgrid)
+
+			lp_er_v = lp_er_v + dt*(
+				VoronoiFVM.lpnorm(sys, tv(tj) - tv_ex, p)^p
 			)
-			l2h1_er = l2h1_er + dt*VoronoiFVM.h1norm(sys, tv(tj) - tv(k))^2
+			lp_er_u = lp_er_u + dt*(
+				VoronoiFVM.lpnorm(sys, tu(tj) - tu_ex, p)^p
+			)
+				#+ VoronoiFVM.lpnorm(sys, tlambda(tj) - tlambda_ex, p)^p
+			
+			l2h1_er = l2h1_er + dt*VoronoiFVM.h1norm(sys, tv(tj) - tv_ex)^2
 		end
 
-		lp_error[i] = (lp_er)^(1/p)
+		lp_error_v[i] = (lp_er_v)^(1/p)
+		lp_error_u[i] = (lp_er_u)^(1/p)
 		l2h1_error[i] = (l2h1_er)^(1/2)
 	end
 
-	K, lp_error, l2h1_error
+	K, lp_error_v, lp_error_u, l2h1_error
 
 end
 
